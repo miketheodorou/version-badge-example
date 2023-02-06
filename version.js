ae9e1524d44e5caea5932f203fd07d31e6aba9d9
Mike Theodorou <mike.theodorou@mlb.com>
2023-02-06 09:50:31 -0700

    updates node version

--- version.js
+++ version.js
@@ -12,10 +12,7 @@ export function getPackageVersion(path) {
 export function generateBadge(branch, version) {
   return fetch(`https://img.shields.io/badge/${branch}-${version}-blue.svg`)
     .then((res) => res.text())
-    .then((svg) => {
-      fs.writeFileSync(`badges/${branch}-version.svg`, svg);
-  
-    })
+    .then((svg) => fs.writeFileSync(`badges/${branch}-version.svg`, svg))
     .catch(console.error);
 }
 
