f48c43b7b88ae23572cfc5c8f0ef9bb3a7e9f33f
Mike Theodorou <mike.theodorou@mlb.com>
2023-02-06 09:46:31 -0700

    fixes esm issues

--- version.js
+++ version.js
@@ -1,5 +1,13 @@
 
 import fs from 'fs';
+import { join } from 'path'
+
+export function getPackageVersion(path) {
+  const packageJson = fs.readFileSync(join(path, 'package.json')).toString();
+  const version = JSON.parse(packageJson).version;
+
+  return version;
+}
 
 export function generateBadge(branch, version) {
   return fetch(`https://img.shields.io/badge/${branch}-${version}-blue.svg`)
